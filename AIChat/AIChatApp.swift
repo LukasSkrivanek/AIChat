b0a7c818746507c6edcf1c814cb0a90b88fe507c
LukasSkrivanek <luky.skrivos@gmail.com>
2024-12-16 11:42:45 +0100

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by macbook on 16.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatTests/AIChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatTests.swift
+//  AIChatTests
+//
+//  Created by macbook on 16.12.2024.
+//
+
+import Testing
+@testable import AIChat
+
+struct AIChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
