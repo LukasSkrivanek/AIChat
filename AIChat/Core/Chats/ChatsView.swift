2f9b5f11dd2a4e8b3865b33d115e4a6aa3a5df46
LukasSkrivanek <luky.skrivos@gmail.com>
2025-01-15 16:11:06 +0100

    [Feature] RecentAvatar

--- AIChat/Core/Chats/ChatsView.swift
+++ AIChat/Core/Chats/ChatsView.swift
@@ -10,9 +10,62 @@ import SwiftUI
 struct ChatsView: View {
     @State private var chats: [ChatModel] = ChatModel.mocks
     @State private var path: [NavigationPathOption] = []
+    @State private var recentAvatars: [AvatarModel] = AvatarModel.mocks
     var body: some View {
         NavigationStack(path: $path) {
             List {
+                if !recentAvatars.isEmpty {
+                    recentSection
+                }
+
+                chatSection
+            }
+            .navigationTitle("Chats")
+            .navigationDestinationForCoreModule(path: $path)
+        }
+    }
+    private var recentSection: some View {
+        Section {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 8) {
+                    ForEach(recentAvatars, id: \.self) { avatar in
+                        if let imageName =  avatar.profileImageName {
+                            VStack(spacing: 8) {
+                                ImageLoaderView(urlString: imageName)
+                                    .aspectRatio(1, contentMode: .fit)
+                                    .clipShape(Circle())
+                                
+                                Text(avatar.name ?? "")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            .anyButton {
+                                onAvatarPress(avatar: avatar)
+                            }
+                        }
+                    }
+                }
+                .padding(.top, 12)
+            
+            }
+            .frame(height: 120)
+            .scrollIndicators(.hidden)
+            .removeListRowFormatting()
+        } header: {
+            Text("Recents")
+        }
+    }
+    private var chatSection: some View {
+        Section {
+            if chats.isEmpty {
+                Text("Your chats will appear here")
+                    .foregroundStyle(.secondary)
+                    .font(.title3)
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .padding(40)
+                    .removeListRowFormatting()
+            } else {
                 ForEach(chats) { chat in
                     ChatRoCellViewBuilder(
                         currentUserId: nil,
@@ -28,14 +81,16 @@ struct ChatsView: View {
                         }
                         .removeListRowFormatting()
                 }
-                
             }
-            .navigationTitle("Chats")
-            .navigationDestinationForCoreModule(path: $path)
+        } header: {
+            Text("Chats")
         }
     }
     private func onChatPress(chat: ChatModel) {
         path.append(.chat(avatarId: chat.avatarId))
+    }
+    private func onAvatarPress(avatar: AvatarModel) {
+        path.append(.chat(avatarId: avatar.avatarId))
     }
 }
 
