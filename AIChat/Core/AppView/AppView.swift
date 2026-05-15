bcc66046c3238f542df6c9930258fafa1adf2ec9
Lukas Skrivanek <Lukas.Skrivanek@adastragrp.com>
2026-05-15 14:05:17 +0200

    feat: app reducer

--- AIChat/Core/AppView/AppView.swift
+++ AIChat/Core/AppView/AppView.swift
@@ -7,89 +7,6 @@
 
 import SwiftUI
 import ComposableArchitecture
-// tabbar - signed in
-// onboarding - signed out
-
-@Reducer
-struct AppReducer {
-    
-    @Dependency(\.userManager)
-    private var userManager
-    @Dependency(\.authManager)
-    private var authManager
-
-    @ObservableState
-    struct AppState: Equatable {
-        @Shared(.appStorage("showTabBar"))
-        var showTabBar = false
-        var isLoading = true
-        var authError: String?
-    }
-
-    enum AppAction: Equatable {
-        case onAppear
-        case showTabBarChanged(Bool)
-        case userStatusCheckSucceeded
-        case userStatusCheckFailed(String)
-    }
-
-    var body: some Reducer<AppState, AppAction> {
-        Reduce { state, action in
-            switch action {
-            case .onAppear:
-                state.isLoading = true
-                
-                return .run { send in
-                    do {
-                        await send(.userStatusCheckSucceeded)
-                    } catch {
-                        await send(.userStatusCheckFailed(error.localizedDescription))
-                    }
-                }
-                
-            case .showTabBarChanged(let showTabBar):
-                
-                // Pokud se uživatel odhlásí, znovu kontrolujeme status
-                if !showTabBar {
-                    return .run { send in
-                        do {
-                            await send(.userStatusCheckSucceeded)
-                        } catch {
-                            await send(.userStatusCheckFailed(error.localizedDescription))
-                        }
-                    }
-                }
-                return .none
-                
-            case .userStatusCheckSucceeded:
-                state.isLoading = false
-                state.authError = nil
-                return .none
-                
-            case .userStatusCheckFailed(let errorMessage):
-                state.isLoading = false
-                state.authError = errorMessage
-                return .run { send in
-                    try await Task.sleep(nanoseconds: 5_000_000_000)
-                    await send(.onAppear)
-                }
-            }
-        }
-    }
-    
-       // MARK: - Private Methods
-
-       private func checkUserStatus() async throws {
-           if let user = authManager.auth {
-                  print("User is authenticated: \(user.uId)")
-                  try await userManager.logIn(auth: user, isNewUser: false)
-              } else {
-                  let result = try await authManager.signInAnonymously()
-                  print("Sign in anonymously: \(result.user.uId)")
-                  try await userManager.logIn(auth: result.user, isNewUser: result.isNewUser)
-              }
-        }
-}
 
 extension UserManager: DependencyKey {
     static let liveValue = UserManager(service: FirebaseUserService())
@@ -124,7 +41,7 @@ struct AppView: View {
                 TabBarView()
             },
             onboardingView: {
-                WelcomeView()
+                WelcomeView(store: <#StoreOf<WelcomeReducer>#>)
             }
         )
         .onAppear {
